a5d3af457515f123b9dfaef855f77b171df4dcc7
Yiru Zhou <110381481+gdgdandsz@users.noreply.github.com>
2024-04-30 05:00:29 -0400

    Add files via upload

--- dl2-master/Model 1 -SimVP/exp.py
+++ dl2-master/Model 1 -SimVP/exp.py
@@ -2,6 +2,7 @@ import os
 import os.path as osp
 import json
 import torch
+import torch.nn as nn
 import pickle
 import logging
 import numpy as np
@@ -87,7 +88,8 @@ class Exp:
         return self.optimizer
 
     def _select_criterion(self):
-        self.criterion = torch.nn.MSELoss()
+        #self.criterion = torch.nn.MSELoss()
+        self.criterion = nn.CrossEntropyLoss()
 
     def _save(self, name=''):
         torch.save(self.model.state_dict(), os.path.join(
